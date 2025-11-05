4eabf4c579fe022ec6dac0f220baa9a36a837f45
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2025-11-05 02:51:43 -0700

    Second Graph - linearised graph

--- Energy Pendulum/energy.py
+++ Energy Pendulum/energy.py
@@ -10,10 +10,44 @@ t = csv['t']
 KE = csv['ke']
 PE = csv['pe']
 TE = csv['te']
-
+lnE = csv['lne']
+'''
 # 1. Energy plot
 plt.figure()
 plt.plot(t, KE, linestyle = '--', label='Kinetic Energy')
 plt.plot(t, PE, linestyle = '', marker = 'o', label='Potential Energy') 
 plt.plot(t, TE, label='Total Energy')
 
+# Aesthetics of the Graph
+plt.xlabel("time (s)", fontsize=16)
+plt.ylabel("Energies (J) ", fontsize=16)
+plt.xticks(fontsize=14, color= (23/2551, 84/255, 128/255))
+plt.yticks(fontsize=16, color="purple")
+plt.legend(loc='upper right', prop={'size': 7})
+plt.title('Graph of Kinetic, Potential, and Total Energies of Pendulum vs Time')
+
+'''
+# 2. Linearise the data
+fitpar, uncert = np.polyfit(t, lnE, 1, cov = 1)
+
+slope = fitpar[0]
+intercept = fitpar[1]
+
+slope_unc = np.sqrt(uncert[0][0])
+intercept_unc = np.sqrt(uncert[1][1])
+
+tau = -1 / slope
+tau_uncert = abs(tau) * (slope_unc / abs(slope))
+
+print(tau)
+print(tau_uncert)
+
+plt.plot(t, lnE, 'bo', label='Data')
+plt.plot(t, slope*t + intercept, 'r-', label=f'Fit: slope = {slope:.3f} ± {slope_unc:.3f}')
+plt.xlabel('Time (s)')
+plt.ylabel('ln(E)')
+plt.xticks(fontsize=14, color= (23/2551, 84/255, 128/255))
+plt.yticks(fontsize=16, color="purple")
+plt.legend(loc='upper right', prop={'size': 7})
+plt.title('Liearized Graph of ln(E) vs Time')
+plt.show()
