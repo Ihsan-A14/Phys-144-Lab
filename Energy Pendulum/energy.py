1151252cf1382d6545e6bf4694e970f6c5b46805
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2025-11-05 00:17:59 -0700

    added the plot for ke, pe and total wrt time

--- Energy Pendulum/energy.py
+++ Energy Pendulum/energy.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  4 23:38:34 2025
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
 
-@author: ihsanmuhammad
-"""
+csv = pd.read_csv('pendulum data.csv')
+
+# Grabbing values from the Table
+t = csv['t']
+KE = csv['ke']
+PE = csv['pe']
+TE = csv['te']
+
+# 1. Energy plot
+plt.figure()
+plt.plot(t, KE, linestyle = '--', label='Kinetic Energy')
+plt.plot(t, PE, linestyle = '', marker = 'o', label='Potential Energy') 
+plt.plot(t, TE, label='Total Energy')
 
