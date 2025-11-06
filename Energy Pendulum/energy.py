65dab8a0a7930514dcb9b4f6abe456cfa915a2d1
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2025-11-06 08:26:53 -0700

    Final commit with data file and python file with the graphs

--- Energy Pendulum/energy.py
+++ Energy Pendulum/energy.py
@@ -11,7 +11,7 @@ KE = csv['ke']
 PE = csv['pe']
 TE = csv['te']
 lnE = csv['lne']
-'''
+
 # 1. Energy plot
 plt.figure()
 plt.plot(t, KE, linestyle = '--', label='Kinetic Energy')
@@ -26,9 +26,8 @@ plt.yticks(fontsize=16, color="purple")
 plt.legend(loc='upper right', prop={'size': 7})
 plt.title('Graph of Kinetic, Potential, and Total Energies of Pendulum vs Time')
 
-'''
 # 2. Linearise the data
-fitpar, uncert = np.polyfit(t, lnE, 1, cov = 1)
+fitpar, uncert = np.polyfit(t, lnE, 1, cov = True)
 
 slope = fitpar[0]
 intercept = fitpar[1]
@@ -36,12 +35,14 @@ intercept = fitpar[1]
 slope_unc = np.sqrt(uncert[0][0])
 intercept_unc = np.sqrt(uncert[1][1])
 
+print(slope, slope_unc)
+
 tau = -1 / slope
 tau_uncert = abs(tau) * (slope_unc / abs(slope))
 
-print(tau)
-print(tau_uncert)
+print('characteristic timescale for energy loss, 𝜏 =', tau ,'±', tau_uncert)
 
+print ('initial Energy =', intercept,'±', intercept_unc)
 plt.plot(t, lnE, 'bo', label='Data')
 plt.plot(t, slope*t + intercept, 'r-', label=f'Fit: slope = {slope:.3f} ± {slope_unc:.3f}')
 plt.xlabel('Time (s)')
@@ -51,3 +52,10 @@ plt.yticks(fontsize=16, color="purple")
 plt.legend(loc='upper right', prop={'size': 7})
 plt.title('Liearized Graph of ln(E) vs Time')
 plt.show()
+
+# 3. FInding time when energy becomes 1/100 initial energy
+import math
+t_1_100 = tau * math.log(100)
+t_1_100_uncert = tau_uncert * math.log(100)  # Propagate uncertainty
+
+print(f"Time to 1/100 energy: {t_1_100:.1f} ± {t_1_100_uncert:.1f} s")
