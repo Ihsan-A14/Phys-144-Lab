df10ea1db780a3324bedadf0d0aeaa470676468e
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2025-11-12 11:59:33 -0700

    Final py file with both parts

--- Spring I/constant.py
+++ Spring I/constant.py
@@ -35,3 +35,43 @@ plt.xlabel('Extension, Δx (m)')
 plt.ylabel('Applied Force, F (N)')
 plt.title('Hooke\'s Law: Applied Force vs. Extension')
 plt.legend(loc='best', prop={'size': 10})
+
+
+# 2. Finding k using the period of oscillation
+csv = pd.read_csv('oscillation.csv')
+
+xaxis = csv['Mass (kg)']
+yaxis = csv['T²/(4π²) (s²)']
+
+plt.plot(xaxis, yaxis, linestyle = ' ', marker = 'o', label = 'Experiment Data')
+
+# Linearizing the graph
+fitpar, uncert = np.polyfit(xaxis, yaxis, 1, cov = True)
+
+a1 = fitpar[0]
+a2 = fitpar[1]
+
+er1 = np.sqrt(uncert[0][0])
+er2 = np.sqrt(uncert[1][1])
+
+print('Slope of the graph :',a1, '±', er1)
+print('Intercept of the graph :', a2, '±', er2)
+
+k = 1/a1
+k_uncert = (er1 / (a1**2))
+
+print('Spring constant :', k, '±', k_uncert)
+
+# Mass of rubber band
+m_s = 3 * k * a2
+m_s_uncert = 3 * k * np.sqrt((er2)**2 + (a2 * k_uncert / k)**2)
+
+print('Mass of rubber band from oscillations:', m_s, '±', m_s_uncert, 'kg')
+
+y_update = a1 * xaxis + a2
+
+plt.plot(xaxis, y_update, linestyle = '-', label = 'Linear Fit Data')
+plt.ylabel('T²/(4π²) (s²)')
+plt.xlabel('Mass (kg)')
+plt.title('Simple Harmonic Motion')
+plt.legend(loc='best', prop={'size': 10})
