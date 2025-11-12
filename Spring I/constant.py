8d58bf849133b92e731526773f5c3110ca8c08d5
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2025-11-12 03:55:37 -0700

    First part - hooke law estimation completed. Next finding k using oscillation

--- Spring I/constant.py
+++ Spring I/constant.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 11 22:35:46 2025
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
 
-@author: ihsanmuhammad
-"""
+# 1. Hooke's Law estimation of spring constant
+csv = pd.read_csv('hooke.csv')
 
+force = csv['Force, F (N)']
+exten = csv['Extension, Δx (m)']
+
+plt.plot(exten, force, linestyle = ' ', marker = 'o', label = 'Experiment Data')
+
+# Linearizing the graph
+fitpar, uncert = np.polyfit(exten, force, 1, cov = True)
+
+a1 = fitpar[0]
+a2 = fitpar[1]
+
+er1 = np.sqrt(uncert[0][0])
+er2 = np.sqrt(uncert[1][1])
+
+print('Slope of the graph :',a1, '±', er1)
+print('Intercept of the graph :', a2, '±', er2)
+
+k = a1
+k_uncert = er1
+
+print('Spring constant :', k, '±', k_uncert)
+
+f_update = a1 * exten + a2
+
+plt.plot(exten, f_update, linestyle = '-', label = 'Linear Fit Data')
+plt.xlabel('Extension, Δx (m)')
+plt.ylabel('Applied Force, F (N)')
+plt.title('Hooke\'s Law: Applied Force vs. Extension')
+plt.legend(loc='best', prop={'size': 10})
